6e2bbdcfd1118650afac32c38e3938ef16f10eeb
juliusvdw <juliusvdw@gmail.com>
2021-12-09 14:52:32 +0800

    Side menu + top nav layout + placeholder content implemented

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -5,7 +5,15 @@ const SideMenu = ({ children }) => {
 
     return (
         <div className = 'side-menu-wrapper' style = {wrapperStyle}>
+            <a href = '/' style = {logoStyle}>J</a>
+            <div id = 'hamburger-icon'>
+            <i class="bi bi-list" style = {iconStyle}></i>
+            </div>
 
+            <div className = 'menu-socials' style = {socialsStyle}>
+            <i className=" mt-3 bi bi-github"></i>
+            <i className=" mt-3 bi bi-linkedin"></i>
+            </div>
         </div>
     )
     
@@ -16,7 +24,31 @@ const wrapperStyle = {
     width:'100px',
     backgroundColor:'#000',
     position:'fixed',
-    borderRight: '1px solid hsla(0, 0%, 100%, 0.2)'
+    borderRight: '1px solid hsla(0, 0%, 100%, 0.2)',
+    paddingTop:'45px',
+    paddingBottom:'45px',
+    display:'flex',
+    flexDirection:'column',
+    alignItems:'center',
+    justifyContent:'space-between'
+}
+
+const logoStyle = {
+    fontSize:'28px',
+    color:'white'
+}
+
+const iconStyle = {
+    fontSize:'34px',
+    color:'white',
+    transition:'opacity 0.3s '
+}
+
+const socialsStyle = {
+    color:'white',
+    fontSize:'22px',
+    display:'flex',
+    flexDirection:'column'
 }
 
 SideMenu.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import { Helmet } from "react-helmet"
 
 import Layout from "../components/layout"
 import SideMenu from "../components/SideMenu"
@@ -9,6 +10,9 @@ import Seo from "../components/seo"
 
 const IndexPage = () => (
   <>
+  <Helmet>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
+  </Helmet>
     <Seo title="Home" />
     <SideMenu />
     <ContentBody />
