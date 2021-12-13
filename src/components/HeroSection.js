82976c6e25bf84117546cd37419923895e4d96f0
juliusvdw <juliusvdw@gmail.com>
2021-12-13 15:55:16 +0800

    Hero section layout complete with placeholder content

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import PropTypes from "prop-types"
 import Particles from "react-tsparticles";
 import ParticlesBackground from "./ParticlesBackground";
@@ -7,20 +7,19 @@ import ParticlesBackground from "./ParticlesBackground";
 
 const HeroSection = ({ children }) => {
     
-    const particlesInit = (main) => {
-        console.log(main);
     
-        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-      };
-    
-      const particlesLoaded = (container) => {
-        console.log(container);
-      };
+   
 
     return (
         <>
         <div className = 'hero-wrapper' style = {heroStyle}>
-            <ParticlesBackground />
+
+            <div className = 'hero-text-container' style = {heroTextStyle}>
+                <p style = {introSmallStyle}>Hi, I'm Julius.</p>
+                <h1 style = {heroNameStyle}>I build things <span className = 'hero-underline' >for the web.</span> </h1>
+                <p style = {subTextStyle}>I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatement.</p>
+                <div className = 'btn btn-primary hero-btn'>View My Projects</div>
+            </div>
         </div>
         </>
     )
@@ -29,8 +28,34 @@ const HeroSection = ({ children }) => {
 
 
 const heroStyle = {
-    height:'cacl(100vh - 80px)'
+    height:'calc(100vh - 80px)'
 
+}
+
+const introSmallStyle = {
+    fontSize: '65px',
+    letterSpacing: '1px',
+    margin:'0px',
+    lineHeigh:'65px'
+}
+
+const heroNameStyle = {
+    fontSize:'65px',
+    letterSpacing:'1px',
+    margin:'0px',
+    lineHeight:'65px'
+}
+
+const heroTextStyle = {
+    paddingTop:'140px'
+}
+
+const subTextStyle = {
+    color:'#9b9b9b',
+    maxWidth:'540px',
+    marginTop:'30px',
+    fontSize:'15px',
+    letterSpacing:'1px'
 }
 
 HeroSection.propTypes = {
