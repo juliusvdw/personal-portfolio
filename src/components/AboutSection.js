701ab4a88616c64d7a6d3273caae5d8a0efc45cc
juliusvdw <juliusvdw@gmail.com>
2021-12-15 14:35:13 +0800

    About section layout added on + placeholder content implemented + image added

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -11,10 +11,24 @@ const AboutSection = ({ children }) => {
             <div className  = 'about-container' style = {containerStyle}>
                 <div className = 'about-text' style = {textContainerStyle}>
                     <h3 style = {aboutHeadingStyle} className = 'd-flex'>About me <hr style = {headingLineStyle} /></h3>
-                    <p style = {aboutSubtextStyle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sunt maiores voluptas aliquid reiciendis labore, quis ipsam ad commodi voluptatibus perspiciatis sint molestias totam neque porro magnam ratione numquam rerum. <br /> <br /> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur qui perferendis cum culpa mollitia excepturi minima cupiditate nam deserunt facere itaque, non ratione saepe earum nobis quo omnis suscipit. Nulla!</p>
+                    <p style = {aboutSubtextStyle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sunt maiores voluptas aliquid reiciendis labore, quis ipsam ad commodi voluptatibus perspiciatis sint molestias totam neque porro magnam ratione numquam rerum. <br /> <br /> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur qui perferendis cum culpa mollitia excepturi.</p>
+                    <h6 className = 'mt-4'>My tech stack</h6>
+                    <div className = 'technologies-container d-flex ' style = {techContainerStyle}>
+                        <div style = {{flex:'1'}}>
+                            <p style = {technologiesStyle}>Javacript</p>
+                            <p style = {technologiesStyle}>React</p>
+                            <p style = {technologiesStyle}>Firebase</p>
+                           
+                        </div>
+                        <div style = {{flex:'1'}}>
+                            <p style = {technologiesStyle}>Webflow</p>
+                            <p style = {technologiesStyle}>Mongo.db</p>
+                            <p style = {technologiesStyle}>Puppeteer</p>
+                        </div>
+                    </div>
                 </div>
-                <div className = 'about-image' style = {imageContainerStyle}>
-
+                <div className = 'about-image-wrapper' style = {imageContainerStyle}>
+                    <img src = '/profile.jpeg' style = {imageStyle} className = 'about-image'></img>
                 </div>
             </div>
         </div>
@@ -37,7 +51,10 @@ const textContainerStyle = {
  flex:'1',
 }
 const imageContainerStyle = {
- flex:'1'
+ flex:'1',
+ justifyContent:'center',
+ alignItems:'center',
+ display:'flex'
 }
 
 const aboutHeadingStyle = {
@@ -57,6 +74,25 @@ const headingLineStyle = {
     marginLeft:'20px',
     color:'#802bb1',
     opacity:'0.7'
+}
+
+const imageStyle = {
+    width:'300px',
+    height:'300px',
+    borderRadius:'7px',
+}
+
+const techContainerStyle = {
+    width:'60%',
+    marginTop:'30px'
+}
+
+const technologiesStyle = {
+    marginTop:'3px',
+    marginBottom:'3px',
+    fontSize:'15px',
+    letterSpacing:'1px',
+    color:'rgb(155,155,155)'
 }
 
 AboutSection.propTypes = {
